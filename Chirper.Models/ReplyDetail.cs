d0703541664c3c9627692657fb5ab50185189bf7
Dinara Abakirova <abakirovada@gmail.com>
2021-09-07 18:35:22 -0400

    Reply models,data, service and controller added

---
+++ Chirper.Models/ReplyDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chirper.Models
+{
+    public class ReplyDetail
+    {
+        public int ReplyId { get; set; }
+        public string Text { get; set; }
+        public Guid AuthorId { get; set; }
+
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+        public virtual Comment Comment { get; set; }
+    }
+}
---
+++ Chirper.Models/ReplyList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chirper.Models
+{
+    public class ReplyList
+    {
+        public int ReplyId { get; set; }
+
+        public string Text { get; set; }
+
+        public Guid AuthorId { get; set; }
+
+        public int CommentId { get; set; }
+
+        public virtual Comment Comment { get; set; }
+    }
+}
