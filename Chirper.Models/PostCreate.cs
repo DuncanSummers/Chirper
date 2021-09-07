59239513efe969949f7800a5526a9965aaec5d63
Duncan Summers <djsummers36@gmail.com>
2021-09-07 16:31:40 -0400

    duncan's branch, Post data, models, service, controller added

---
+++ Chirper.Models/PostCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chirper.Models
+{
+    public class PostCreate
+    {
+        [Required]
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
